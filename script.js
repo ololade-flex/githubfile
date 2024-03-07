45047b99580a1f75e7bb5fc4b6e569a188d674c2
ololade-flex <114015777+ololade-flex@users.noreply.github.com>
2024-03-07 14:48:21 +0100

    Calculator

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const toggle = document.querySelector('.toggle')
-const navigation = document.querySelector('.navigation')
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
 
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active')
-  navigation.classList.toggle('active')
-})
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    var expression = document.getElementById('display').value;
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+}
+
